175ccce9cc8b53306c98e5cf548d789b79e1346f
AntonVopilov <50808154+AntonVopilov@users.noreply.github.com>
2019-05-20 15:47:06 +0300

    Add files via upload
    Являются ли строки перестановками

---
+++ hw_vopilov.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Реализуйте метод, определяющий, является ли одна строка
+перестановкой другой. Под перестановкой понимаем любое
+изменение порядка символов. Регистр учитывается, пробелы
+являются существенными.
+"""
+
+
+def is_permutation(a: str, b: str) -> bool:
+    """проверяет, являются ли строчки 'a' и 'b' перестановками"""
+
+    if len(a) != len(b):
+        return False
+
+    if sorted(a) == sorted(b):
+        return True
+    else:
+        return False
+
+
+assert is_permutation('baba', 'abab')
+assert is_permutation('abbba', 'abab')
